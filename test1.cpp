abb5d65a627c839a94a00070dfbd615746e77f6b
markotymofeiuk <marko.tymofeiuk.shi.2023@lpnu.ua>
2023-10-05 22:04:16 +0300

    Check out test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int main(){
     cout << "Hello y'all";
 }
+
+
+
+            88                                                
+            ""                                                
+                                                              
+8b,dPPYba,  88  ,adPPYb,d8  ,adPPYb,d8  ,adPPYba, 8b,dPPYba,  
+88P'   `"8a 88 a8"    `Y88 a8"    `Y88 a8P_____88 88P'   "Y8  
+88       88 88 8b       88 8b       88 8PP""""""" 88          
+88       88 88 "8a,   ,d88 "8a,   ,d88 "8b,   ,aa 88          
+88       88 88  `"YbbdP"Y8  `"YbbdP"Y8  `"Ybbd8"' 88          
+                aa,    ,88  aa,    ,88                        
+                 "Y8bbdP"    "Y8bbdP"          
